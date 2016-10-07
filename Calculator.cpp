f6e1c6c3b9d6d4d5f91d96549506dd771c99d267
danil4157 <iameagle@protonmail.com>
2016-10-07 15:42:35 +0400

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -19,7 +19,12 @@ int main () {
     cout << "Сумма а и b = " << summary (a,b) << endl;
     cout << "Разность a и b = " << subtraction (a,b) << endl;
     cout << "Произведение a и b = " << multiplication (a,b) << endl;
-    cout << "Частное a и b = " << division (a,b) << endl;
+    if (b != 0) {
+        cout << "Частное a и b = " << division (a,b) << endl;
+    }
+    else {
+        cout << "Ошибка. На 0 делить нельзя\n";
+    }
     cout << "a в степени с = " << involution (a,c) << endl;
     cout << "Квадратный корень из числа a = " << square_root (a) << endl;
     cout << "Абсолютное значение d = " << absolute (d) << endl;
@@ -42,7 +47,7 @@ double division (float a, float b) {
         return a / b;
     }
     else {
-        return 0;
+        return -1;
     }
 }
 
